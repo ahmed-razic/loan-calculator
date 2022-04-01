06139b6efb4385643dd7a245a726142470b99489
ahmed-razic <ahmed.razic@gmail.com>
2022-04-01 18:02:52 +0200

    variabes

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById('loan-form').addEventListener('submit', calculate);
+
+function calculate(e) {
+  e.preventDefault();
+
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+}
