039dc456e8f67f7e844b130c01a800bc2d591277
ahmed-razic <ahmed.razic@gmail.com>
2022-04-01 18:30:37 +0200

    calculate loan

--- app.js
+++ app.js
@@ -9,4 +9,36 @@ function calculate(e) {
   const monthlyPayment = document.getElementById('monthly-payment');
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
+
+  const principle = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  //monthly payment
+  const m = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principle * m * calculatedInterest) / (m - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principle).toFixed(2);
+  } else {
+    showError('Please check numbers');
+  }
+}
+
+function showError(error) {
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  const errorDiv = document.createElement('div');
+  errorDiv.className = 'alert alert-danger';
+
+  errorDiv.appendChild(document.createTextNode(error));
+
+  card.insertBefore(errorDiv, heading);
+
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 3000);
 }
